0451d13ab838a6b4b0ac128b2202d02ffd6cd367
flyman <450233952@qq.com>
2022-06-08 10:18:31 +0800

    完成编程题6

---
+++ src/com/flyman/test/q6.java
@@ -0,0 +1,10 @@
+package com.flyman.test;
+
+public class q6 {
+    public static void main(String[] args) {
+        String s = "I am is test the charAt(),thanks!";
+        System.out.println("字符串为：" + s);
+        System.out.println("第一个字符为：" + s.charAt(0));
+        System.out.println("最后一个字符为：" + s.charAt(s.length() -1));
+    }
+}
