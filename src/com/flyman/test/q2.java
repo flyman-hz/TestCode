5ea1ba2f2d435cbc19a81578cfd0cbe545ae5a6c
flyman <450233952@qq.com>
2022-06-08 09:27:44 +0800

    编程题第二题完成

--- src/com/flyman/test/q2.java
+++ src/com/flyman/test/q2.java
@@ -2,7 +2,9 @@ package com.flyman.test;
 
 public class q2 {
     public static void main(String[] args) {
-
+        AdvertisementBoard adv = new AdvertisementBoard();
+        adv.show(new WhiteCloudCorp());
+        adv.show(new BlackLandCorp());
     }
 }
 interface Advertisement {
@@ -10,15 +12,40 @@ interface Advertisement {
     public String getCorpName();
 }
 
+class AdvertisementBoard {
+    public void show(Advertisement advertisement) {
+        System.out.println(advertisement.getCorpName() + "广告词如下：");
+        advertisement.showAdvertisement();//显示广告词
+    }
+}
+
 class WhiteCloudCorp implements Advertisement {
 
     @Override
     public void showAdvertisement() {
-
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println("飞机中的战斗机");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@");
     }
 
     @Override
     public String getCorpName() {
         return "白云有限公司";
+    }
+}
+class BlackLandCorp implements Advertisement {
+
+    @Override
+    public void showAdvertisement() {
+        System.out.println("-----------------");
+        System.out.println("劳动是爹");
+        System.out.println("劳动是妈");
+        System.out.println("abb");
+        System.out.println("-----------------");
+    }
+
+    @Override
+    public String getCorpName() {
+        return "黑土集团";
     }
 }
